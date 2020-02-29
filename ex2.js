a4050414abe29fd50391c62272ccb7d271356334
guiferrini <guilhermemferrini@gmail.com>
2020-02-29 22:52:45 +0100

    ex2, Add dividir

--- ex2.js
+++ ex2.js
@@ -21,5 +21,13 @@ function multiplicar() {
     let texto = "A soma entre " + n1 + " e " + n2 + " é igual a " + operacao + ".";
     document.getElementById("ola").innerText = texto;
 }
+
+function dividir() {
+    var n1 = Number(document.getElementById("num1").value);
+    var n2 = Number(document.getElementById("num2").value);
+    var operacao = n1 / n2;
+    let texto = "A soma entre " + n1 + " e " + n2 + " é igual a " + operacao + ".";
+    document.getElementById("ola").innerText = texto;
+}
     //document.getElementById("num1").innerText = num1; p escrever o n° na tela
     //document.getElementById("num2").innerText = num2;
