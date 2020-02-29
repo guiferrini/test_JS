4aca5ffb55733f543eb3d85981f8cef7a4426228
guiferrini <guilhermemferrini@gmail.com>
2020-02-29 22:49:38 +0100

    ex2, Add Somar e Subatrair

--- ex2.js
+++ ex2.js
@@ -1,11 +1,17 @@
-function calcular() {
+function somar() {
     var n1 = Number(document.getElementById("num1").value);
     var n2 = Number(document.getElementById("num2").value);
-
-    var soma = n1 + n2;
-
-    //document.getElementById("num1").innerText = num1; p escrever o n° na tela
-    //document.getElementById("num2").innerText = num2;
-    let texto = "A soma entre " + n1 + " e " + n2 + " é igual a " + soma + ".";
+    var operacao = n1 + n2;
+    let texto = "A soma entre " + n1 + " e " + n2 + " é igual a " + operacao + ".";
     document.getElementById("ola").innerText = texto;
 }
+
+function subtrair() {
+    var n1 = Number(document.getElementById("num1").value);
+    var n2 = Number(document.getElementById("num2").value);
+    var operacao = n1 - n2;
+    let texto = "A soma entre " + n1 + " e " + n2 + " é igual a " + operacao + ".";
+    document.getElementById("ola").innerText = texto;
+}
+    //document.getElementById("num1").innerText = num1; p escrever o n° na tela
+    //document.getElementById("num2").innerText = num2;
