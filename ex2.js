492959a290bc295c5abf4c2dc809583d54318c15
guiferrini <guilhermemferrini@gmail.com>
2020-02-29 22:51:39 +0100

    ex2, Add multiplicacao

--- ex2.js
+++ ex2.js
@@ -13,5 +13,13 @@ function subtrair() {
     let texto = "A soma entre " + n1 + " e " + n2 + " é igual a " + operacao + ".";
     document.getElementById("ola").innerText = texto;
 }
+
+function multiplicar() {
+    var n1 = Number(document.getElementById("num1").value);
+    var n2 = Number(document.getElementById("num2").value);
+    var operacao = n1 * n2;
+    let texto = "A soma entre " + n1 + " e " + n2 + " é igual a " + operacao + ".";
+    document.getElementById("ola").innerText = texto;
+}
     //document.getElementById("num1").innerText = num1; p escrever o n° na tela
     //document.getElementById("num2").innerText = num2;
