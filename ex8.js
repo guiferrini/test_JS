4e712d694fbd3f448a28a52f6970476a11f7fd62
guiferrini <guilhermemferrini@gmail.com>
2020-03-03 14:08:46 +0100

    add ex 8, n°s primos

---
+++ ex8.js
@@ -0,0 +1,23 @@
+function gerar() {
+    let n = document.getElementById("txt").value;
+    cont = 0;
+    c = 1;
+    while (c <= n) {
+        if (n % c == 0) {
+            cont += 1;
+            c++
+        } else {
+            c++
+        }
+    }
+    if (cont == 2) {
+        document.getElementById("resp").innerHTML = "número primo";  
+    } else {
+        document.getElementById("resp").innerHTML = "número NÃO primo";
+    }
+
+    
+
+}
+/*    se ele eh / por ele e 1 é primo se ele é divi por + de 3 naõ é primo...   
+ */
