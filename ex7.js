3f16e1707bb7e13403d917a7f8dd5395a1b6446b
guiferrini <guilhermemferrini@gmail.com>
2020-03-04 17:35:40 +0100

    ex7, gera primeira letar em codigo

--- ex7.js
+++ ex7.js
@@ -7,12 +7,18 @@ LVL 2:  Testear con: Champiñones
 LVL 3: Testear con: Hola Mundo!
 
 LVL 4: Testear con: Murciélagos en València
+
+LVL 5: Sin Ninguna Función
  */
 
  function gerar() {
     let palavra = document.getElementById("txt").value;
-
-    gerar uma palavra em codigo e depois o codigo em maiuscula
+    // teste -> document.getElementById("resp").innerText = palavra;
+    //for (let c=0; c <= length.palavra; c++) {
+    var x = palavra.charCodeAt(0); //gera 1° letra em ordem
+    document.getElementById("resp").innerHTML = "The Unicode value is: " + x;
+    
+    /*gerar uma palavra em codigo e depois o codigo em maiuscula
     String.fromCharCode(65, 66, 67)
-    toString...
+    toString...*/
  }
