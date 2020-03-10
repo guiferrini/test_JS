fc7f39dee67297db733a2625822bed4c7430260e
guiferrini <guilhermemferrini@gmail.com>
2020-03-10 15:54:22 +0100

    ex11, add al coche funcionalidades: corer, frenar y regargar. El coche se mueve. Tiene corrida!

--- ex11.poo.Carrera.Coches.js
+++ ex11.poo.Carrera.Coches.js
@@ -7,27 +7,39 @@ class Circuito {
 
 let prova =  new Circuito (500);
 
-class Coche {
+//----------------------
+
+class Coche extends Circuito {
 
     constructor(kmvuelta, velocidad, gasolina) {
-        super (kmvuelta);
+        super(kmvuelta);
         this.velocidad = velocidad;
         this.gasolina = gasolina;
     }
-}
-
-
-
-class Coche extends Circuito {
 
     correr() {
         if (this.kmvuelta == 300) {
             console.log("Prova finalizada!");
+        } else if (this.gasolina == 0) {
+            console.log("vc n tem gasolina")
         } else {
             this.velocidad += 80;
             this.kmvuelta += 50;
-            this.gasolina -= 2;
+            this.gasolina -= 10;
             console.log(this.kmvuelta, this.velocidad, this.gasolina);
+        }
+    }
+
+    frenar() {
+        this.velocidad = 0;
+        console.log(this.velocidad)
+    }
+
+    recargar() {
+        if (this.velocidad == 0) {
+            this.gasolina += 40;
+        } else {
+            console.log("vc tem q parar para recargar!")
         }
     }
 }
